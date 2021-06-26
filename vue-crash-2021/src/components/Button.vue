0fefccdb688814ae60ccafcad2d5ebc68769d0f2
Aline Oliveira <oliveira.alineod@gmail.com>
2021-06-26 19:29:56 -0300

    Adicionando Botão

---
+++ vue-crash-2021/src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    @:click="onClick()"
+    v-bind:style="{ backgroundColor: color }"
+    class="btn"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onclick() {
+      console.log("click");
+    },
+  },
+};
+</script>
+<style scoped></style>
