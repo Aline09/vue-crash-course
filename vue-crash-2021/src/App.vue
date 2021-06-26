80eb8c1a0853c460328284557fa3ea5683bb3ea8
Aline Oliveira <oliveira.alineod@gmail.com>
2021-06-26 19:20:05 -0300

    Criando Componente Header

--- vue-crash-2021/src/App.vue
+++ vue-crash-2021/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <Header title="kjkj" />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
+  components: {
+    Header,
+  },
 };
 </script>
 
