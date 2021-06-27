e6a01baf425be8aa92c7614ba5188e4353535f49
Aline Oliveira <oliveira.alineod@gmail.com>
2021-06-27 11:15:23 -0300

    Emitindo Eventos

--- vue-crash-2021/src/App.vue
+++ vue-crash-2021/src/App.vue
@@ -1,15 +1,52 @@
 <template>
   <div class="container">
     <Header title="kjkj" />
+    <Tasks @delete-task="deleteTask" v-bind:tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((el) => el.id !== id);
+      }
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3rd at 11:00pm",
+        reminder: false,
+      },
+    ];
   },
 };
 </script>
