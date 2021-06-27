c4afe70a928abe4f3f60c5220154a2501986a417
Aline Oliveira <oliveira.alineod@gmail.com>
2021-06-27 13:30:00 -0300

    Adiciona Tarefas

--- vue-crash-2021/src/App.vue
+++ vue-crash-2021/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Header title="kjkj" />
+    <AddTask @add-task="addTask" />
     <Tasks
       @toggle-reminder="toggleReminder"
       @delete-task="deleteTask"
@@ -12,11 +13,13 @@
 <script>
 import Header from "./components/Header.vue";
 import Tasks from "./components/Tasks.vue";
+import AddTask from "./components/AddTask.vue";
 export default {
   name: "App",
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
@@ -24,6 +27,10 @@ export default {
     };
   },
   methods: {
+    addTask(task) {
+      console.log(task);
+      this.tasks = [...this.tasks, task];
+    },
     deleteTask(id) {
       if (confirm("Are you sure?")) {
         this.tasks = this.tasks.filter((el) => el.id !== id);
